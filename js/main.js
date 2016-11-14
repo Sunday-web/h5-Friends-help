03775122d0400d16843bcb43c6931aa5a6a1ff3c
Sunday <775011185@qq.com>
2016-11-14 15:12:35 +0800

    Add file via Sunday
    v1.10

---
+++ js/main.js
@@ -0,0 +1,9 @@
+﻿function showShareBox(){		//打开分享窗口
+    document.getElementById('sharebox').style.display="block";
+}
+function closeSharebox(){		//关闭分享窗口
+    document.getElementById('sharebox').style.display="none";
+}
+function help(){
+    alert("您成功为您的好友助力！");
+}
